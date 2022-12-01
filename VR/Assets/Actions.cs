7e5e375f9404456e60e250d7413ce1b940aa2576
Serap98 <61949231+Serap98@users.noreply.github.com>
2022-12-01 10:10:38 +0200

    update

--- VR/Assets/Actions.cs
+++ VR/Assets/Actions.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 public class Actions : MonoBehaviour
 {
 	public GameObject grass_obj;
+	public GameObject badGrass;
+	public GameObject grass_obj_growed;
+	public GameObject uncutTree;
+	public GameObject cutTree;
+	public GameObject plank;
+	public GameObject fireStone;
 	//private GameObject originalGameObject;// = GameObject.Find("Camera");
 	private GameObject holdPositionObject;// = originalGameObject.transform.GetChild(0).gameObject;
     // Start is called before the first frame update
@@ -14,7 +20,16 @@ public class Actions : MonoBehaviour
 	private bool firstGrowthCompleted = false; //seeds
 	private bool secondGrowthCompleted = false; //watering
 	private bool wateringCanHasWater = false;
+	private bool badGrassRemoved = false;
+	private bool badGrassFinishedGrowth = false;
+	private bool thirdGrowthCompleted = false;
 	private int clickedTimes;
+	private bool collidedWithTree = false;
+	private bool treeIsCut = false;
+	private bool boilerHasLog = false;
+	private bool collidedWithBoiler = false;
+	private bool boilerIsBurning = false;
+	
     void Start()
     {
         //originalGameObject = GameObject.Find("Camera");
@@ -35,7 +50,7 @@ public class Actions : MonoBehaviour
 			child = null;
 		}
 		// First growth
-        if (collidedWithMud && child.name == "PotionRed" && !firstGrowthCompleted){
+        if (collidedWithMud && child != null&& child.name == "PotionRed" && !firstGrowthCompleted){
 			if (Input.GetKeyDown(KeyCode.G) && grass_obj.transform.position.y <= 0)
 			{
 				Debug.Log("Collided, and in mud and pressed G");
@@ -45,7 +60,7 @@ public class Actions : MonoBehaviour
 			else if(grass_obj.transform.position.y >= 0)
 				firstGrowthCompleted = true;
 		}
-
+		// first watering
 		if (collidedWithMud && child.name == "WateringCan_01" && firstGrowthCompleted && !secondGrowthCompleted && wateringCanHasWater){
 			Debug.Log("Collided with mud with wateringCan and water");
 			
@@ -54,13 +69,107 @@ public class Actions : MonoBehaviour
 
 				clickedTimes++;
 				Debug.Log("Collided with watercan Growing");
-				grass_obj.transform.localScale  += new Vector3(0.1f, 0.05f, 0f);
+				//grass_obj.transform.localScale  += new Vector3(0.1f, 0.05f, 0f);
+				for(int i = 0; i < grass_obj.transform.childCount; i++)
+				{
+					GameObject Go = grass_obj.transform.GetChild(i).gameObject;
+					Go.transform.localScale +=  new Vector3(0.15f, 0.15f, 0f);
+				}
 				
 			}
 			else if (clickedTimes > 4)
 			{
 				wateringCanHasWater = false;
 				secondGrowthCompleted = true;
+				clickedTimes = 0;
+			}
+		}
+		
+		// Bad grass growth
+		if (!badGrassRemoved && !badGrassFinishedGrowth && secondGrowthCompleted && !thirdGrowthCompleted)
+		{
+			//badGrass.transform.position += new Vector3(0f, 0.05f, 0f);
+			if (badGrass.transform.position.y <= 0)
+			{
+				Debug.Log("Collided, and in mud and pressed G");
+				badGrass.transform.position += new Vector3(0f, 0.15f, 0f) * Time.deltaTime;
+				
+			}
+			else
+			{
+				badGrassFinishedGrowth = true;
+			}
+		
+		}
+		if (!badGrassRemoved && badGrassFinishedGrowth && secondGrowthCompleted && !thirdGrowthCompleted && child != null&& child.name == "PotionBlue" &&  collidedWithMud && Input.GetKeyDown(KeyCode.G))
+		{
+			badGrass.transform.position -= new Vector3(0f, 0.5f, 0f);
+			if (badGrass.transform.position.y <= -1.5) {
+				badGrassRemoved = true;
+				GameObject.Destroy(badGrass);
+			}
+		}
+		
+		// 2nd watering
+		if (collidedWithMud && child.name == "WateringCan_01" && !thirdGrowthCompleted && wateringCanHasWater && badGrassRemoved){
+			Debug.Log("Collided with mud with wateringCan and water 2d watering");
+			
+			if (Input.GetKeyDown(KeyCode.G) && clickedTimes <= 4)
+			{
+
+				clickedTimes++;
+				Debug.Log("Collided with watercan Growing 2nd watering");
+				//grass_obj.transform.localScale  += new Vector3(0.1f, 0.05f, 0f);
+			
+				
+			}
+			else if (clickedTimes > 4)
+			{
+				wateringCanHasWater = false;
+				thirdGrowthCompleted = true;
+				
+				grass_obj_growed.transform.position += new Vector3(0f, 2f, 0f);
+				grass_obj.transform.position -= new Vector3(0f, 2f, 0f);
+				clickedTimes = 0;
+			}
+		}
+		// chopping tree
+		if (collidedWithTree && child != null && child.name == "Axe" && !treeIsCut)
+		{
+			Debug.Log("Collided with tree while with axe");
+			
+			if (Input.GetKeyDown(KeyCode.G) && clickedTimes <= 4)
+			{
+				clickedTimes++;
+			}
+			else if (clickedTimes > 4)
+			{
+				treeIsCut = true;
+				uncutTree.transform.position -= new Vector3(0f, 10f, 0f);
+				cutTree.transform.position += new Vector3(0f, 2f, 0f);
+				plank.transform.position = new Vector3(0f, 0f, 0f);
+				//plank.isKinematic = false;
+				// plank
+				
+			}
+		}
+		// adding log to boiler
+		if (collidedWithBoiler && !boilerHasLog && child != null && child.name == "Plank")
+		{
+			Debug.Log("Collided with boiled with log");
+			if (Input.GetKeyDown(KeyCode.G))
+			{
+				boilerHasLog = true;
+				GameObject.Destroy(child);
+				
+			}
+		}
+		if (collidedWithBoiler && child != null && child.name == "FireStone" && boilerHasLog && !boilerIsBurning) {//fireStone
+		if (Input.GetKeyDown(KeyCode.G))
+			{
+				boilerIsBurning = true;
+				Debug.Log("Boiler is burning");
+				
 			}
 		}
     }
@@ -82,6 +191,21 @@ public class Actions : MonoBehaviour
 			wateringCanHasWater = true;
 			Debug.Log("Watering can has water");
 		}
+		
+		if (other.gameObject.name == "TreeUncut") {
+			collidedWithTree = true;
+			Debug.Log("Collided with uncut tree");
+		}
+		else
+			collidedWithTree = false;
+		
+		//collidedWithBoiler
+		if (other.gameObject.name == "Boiler1") {
+			collidedWithBoiler = true;
+			Debug.Log("Collided with boiler");
+		}
+		else
+			collidedWithBoiler = false;
 
     }  
 }
